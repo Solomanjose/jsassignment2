e490ee2807fe26f290ce4da54cefaf6a28a0fc5b
SolomanJose <solomanjose11@gmail.com>
2024-06-19 21:02:23 +0530

    pgm6 don

--- script.js
+++ script.js
@@ -78,4 +78,19 @@ function changeTextColor() {
 }
 
 
+// 6.Write a JavaScript function to get the values of First and Last names of the following form.
+// Sample HTML file :
+// <!DOCTYPE html>
+// <html><head>
+// <meta charset=utf-8 />
+// <title>Return first and last name from a form - w3resource</title>
+// </head><body>
+// <form id="form1">
+// First name: <input type="text" name="fname" value="David"><br>
+// Last name: <input type="text" name="lname" value="Beckham"><br>
+// <input type="submit" value="Submit">
+// </form>
+// </body>
+// </html>
+
 
